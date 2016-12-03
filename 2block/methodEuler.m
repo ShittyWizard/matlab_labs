2050c182fb89736d0c7bcb50c3bfa814acb02a85
Vladislav Anisimov <anisimov.develop@gmail.com>
2016-12-03 03:20:17 +0300

    Added 2nd block code of programs
    methodRunge -  method of Runge-Kutta
    methodEuler - method of Euler

---
+++ 2block/methodEuler.m
@@ -0,0 +1,31 @@
+function methodEuler
+    a = 1;
+    b = 5;
+    n = 100;
+    y0 = 1/exp(1);
+    [x, y] = euler(a,b,y0,n);
+    
+    x1 = 1 : 0.01 : 5;
+    y1 = x1.^2 .* exp(-x1);
+    
+    plot(x,y);
+    hold on;
+    plot(x1,y1,'r');
+   
+    grid on;
+
+function [x,y] = euler(a,b,y0,n)
+    h = (b - a)/n;
+    x(1) = a;
+    y(1) = y0;
+    for i = 2 : n
+        x(i) = x(i-1) + h;
+        y(i) = y(i-1) +h*f(x(i),y(i-1));
+    end
+    
+    
+function y = f(x,y)
+    y = (3*x^2*exp(-x) - (x+1)*y)/x;
+    
+    
+   
---
+++ 2block/methodRunge.m
@@ -0,0 +1,34 @@
+function methodRunge
+    a = 1;
+    b = 5;
+    y0 = 1/exp(1);
+    n = 5;
+    [x, y] = runge(a,b,y0,n);
+    
+    x1 = 1 : 0.01 : 5;
+    y1 = x1.^2 .* exp(-x1);
+    plot(x,y);
+    hold on;
+    plot(x1,y1,'r');
+   
+    grid on;
+    
+    
+function[x,y] = runge(a,b,y0,n)
+    h = (b - a)/n;
+    x(1) =a;
+    y(1) = y0;
+    for i = 1 : n
+        k1 = f(x(i), y(i));
+        k2 = f(x(i) +h/2, y(i) +h*k1/2);
+        k3 = f(x(i) + h/2, y(i) + h *k2/2);
+        k4 = f(x(i) + h, y(i) + h*k3);
+        y(i+1) = y(i) + h*(k1 + 2*k2 + 2*k3 + k4)/6;
+        x(i+1) = x(i) + h;
+    end
+
+
+
+function y = f(x,y)
+    y = (3*x^2*exp(-x) - (x+1)*y)/x;
+    
